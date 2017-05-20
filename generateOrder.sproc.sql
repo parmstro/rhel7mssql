4bba19f0aaf5e15e97e9a3d02ebe09ef7a9af91a
Paul Armstrong <parmstro@redhat.com>
2017-05-20 02:14:55 -0400

    modified generateOrder.sproc.sql

--- generateOrder.sproc.sql
+++ generateOrder.sproc.sql
@@ -1,3 +1,6 @@
+DROP PROCEDURE testschema.generateOrder
+GO
+
 CREATE PROCEDURE testschema.generateOrder 
 AS
 BEGIN
@@ -10,46 +13,47 @@ DECLARE @EmployeeID INT        -- the employee that made the order
 DECLARE @CustomerID INT        -- the customer that gets the order
 DECLARE @OrderID INT           -- the orderID for the items to be attached to
 DECLARE @ItemID INT            -- the itemID of the item to order
+DECLARE @MeasureID INT         -- the measurement units that the quantity of the order item is in.
 DECLARE @countItems INT        -- random number of items in the order
-DECLARE @count INT 	       -- an iterator
-
+DECLARE @counter INT 	       -- an iterator
+DECLARE @Qty INT					 -- the number of items of a given itemid to be ordered.
 
 -- Generate a random order of items for a random customer and assign it to a random employee
-SET @totalEmployees = SELECT COUNT(*) FROM testschema.employees
-SET @totalCustomers = SELECT COUNT(*) FROM testschema.customers
-SET @totalItems = SELECT COUNT(*) FROM testschema.items
+SET @totalEmployees = (SELECT COUNT(*) FROM testschema.employees)
+SET @totalCustomers = (SELECT COUNT(*) FROM testschema.customers)
+SET @totalItems = (SELECT COUNT(*) FROM testschema.items)
 
 WHILE (@EmployeeID IS NULL)
   BEGIN
-    SET @EmployeeID = SELECT id FROM testschema.employees WHERE id = (SELECT FLOOR(RAND()*(@totalEmployees-1)+1))
+    SET @EmployeeID = (SELECT id FROM testschema.employees WHERE id = (SELECT FLOOR(RAND()*(@totalEmployees-1)+1)))
   END
 
 WHILE (@CustomerID IS NULL)
   BEGIN
-    SET @CustomerID = SELECT id FROM customers WHERE id = (SELECT FLOOR(RAND()*(@totalCustomers-1)+1))
+    SET @CustomerID = (SELECT id FROM customers WHERE id = (SELECT FLOOR(RAND()*(@totalCustomers-1)+1)))
   END
 
-SELECT @countItems = SELECT FLOOR(RAND()*(20-1)+1)
+SET @countItems = (SELECT FLOOR(RAND()*(20-1)+1))
 
 BEGIN TRANSACTION
   INSERT INTO testschema.orders (EmployeeID, CustomerID, DateOrdered) VALUES
-  (@EmployeeID, @CustomerID, Now())
-  SET @OrderID = @@ROWCOUNT
-  SET @count = 1
-  WHILE (@count <= @countItems)
+  (@EmployeeID, @CustomerID, GETDATE())
+  SET @OrderID = (SELECT @@IDENTITY)
+  SET @counter = 1
+  WHILE (@counter <= @countItems)
     BEGIN
       WHILE (@ItemID IS NULL)
         BEGIN
-          SET @ItemID = SELECT id FROM testschema.items WHERE id = (SELECT FLOOR(RAND()*(@totalItems-1)+1))
+          SET @ItemID = (SELECT id FROM testschema.items WHERE id = (SELECT FLOOR(RAND()*(@totalItems-1)+1)))
         END -- WHILE
-      SET @MeasureID = SELECT measureid FROM testschema.items WHERE itemid = @ItemID
-      SET @Qty = SELECT FLOOR(RAND()*(20-1)+1)
-      INSERT INTO testschema.orderdetails (OrderID, Qty, MeasureID) VALUES
-      (@OrderID, @Qty, @MeasureID)
-      SET @count = @count + 1
+      SET @MeasureID = (SELECT measureid FROM testschema.items WHERE id = @ItemID)
+      SET @Qty = (SELECT FLOOR(RAND()*(20-1)+1))
+      INSERT INTO testschema.orderdetails (OrderID, Qty, MeasureID, ItemID) VALUES
+      (@OrderID, @Qty, @MeasureID, @ItemID)
+      SET @counter = @counter + 1
+      SET @ItemID = NULL
     END -- WHILE
 COMMIT TRANSACTION
 
 END -- SPROC
 ;
-GO
